89401753c1defad36d270dc426f48df4c2a4c948
vdornadula <vaishnavidornadula2026@u.northwestern.edu>
2024-01-22 13:07:34 -0600

    checking if i get the same output with the files not in the directory

--- input_test.py
+++
@@ -1,4 +0,0 @@
-import input_validity_test_main;
-
-def test_input():
-    assert input_validity_test_main.test_validity() == True
--- input_validity_test_main.py
+++
@@ -1,9 +0,0 @@
-from check_init_pose_validity import InputFiles
-import os
-
-async def test_validity():
-    print(os.getcwd())
-    return True
-
-# if __name__ == "__main__":
-#     asyncio.run(main())
