e97b01cc90a21eedabd19e13e9b6b219caa020d4
vdornadula <vaishnavidornadula2026@u.northwestern.edu>
2024-04-30 15:52:13 -0500

    working on getting validity tested

---
+++ input_validity_test_main.py
@@ -0,0 +1,14 @@
+from check_init_pose_validity import InputFiles
+import os
+
+async def test_validity():
+    print("in testing inputs")
+    # navigate to code queue folder
+    # for each folder in code queue?
+        # load in folder
+        # check validity
+
+    return True
+
+# if __name__ == "__main__":
+#     asyncio.run(main())
