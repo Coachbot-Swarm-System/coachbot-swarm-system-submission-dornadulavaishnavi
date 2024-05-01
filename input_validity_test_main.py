153d171064354ed59fc74c25fb839564bff74167
vdornadula <vaishnavidornadula2026@u.northwestern.edu>
2024-05-01 09:40:00 -0500

    changing to assert

--- input_validity_test_main.py
+++ input_validity_test_main.py
@@ -7,7 +7,8 @@ def test_validity():
     input_files = InputFiles("Vaish_Flocking")
     print("Files Loaded Correctly:", input_files.files_loaded_correctly, "Valid initial positions given", input_files.init_poses_valid)
     print(input_files.files_loaded_correctly and input_files.init_poses_valid)
-    return input_files.files_loaded_correctly and input_files.init_poses_valid
+    assert input_validity_test_main.files_loaded_correctly == True and input_files.init_poses_valid == True
+    # return input_files.files_loaded_correctly and input_files.init_poses_valid
     # for each folder in code queue?
         # load in folder
         # check validity
