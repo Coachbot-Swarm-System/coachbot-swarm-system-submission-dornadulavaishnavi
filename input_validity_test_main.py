a0cd5e6458a080a580a484e735bd4270417fb28c
vdornadula <vaishnavidornadula2026@u.northwestern.edu>
2024-05-01 17:39:06 -0500

    other print statements getting in the way

--- input_validity_test_main.py
+++ input_validity_test_main.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 def test_validity():
-    print("in testing inputs")
+    # print("in testing inputs")
     # navigate to code queue folder
     input_files = InputFiles("Vaish_Flocking")
     print("Files Loaded Correctly:", input_files.files_loaded_correctly, "Valid initial positions given", input_files.init_poses_valid)
@@ -11,10 +11,11 @@ def test_validity():
     # assert input_files.is_input_valid() == True, "Input files have an error"
     # return input_files.files_loaded_correctly and input_files.init_poses_valid
     if input_files.files_loaded_correctly and input_files.init_poses_valid:
-        sys.stdout.write('1')
+        # sys.stdout.write('1')
+        print('Passed Autograder Test')
         return True
     else:
-        sys.stdout.write('0')
+        sys.stdout.write('Failed')
         return False
     # for each folder in code queue?
         # load in folder
