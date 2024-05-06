1a8a08baa80c1aa67c0dedfabca69fc815d34ec4
vdornadula <vaishnavidornadula2026@u.northwestern.edu>
2024-05-06 15:02:18 -0500

    might have had an extra slash

--- get_files.py
+++ get_files.py
@@ -2,7 +2,7 @@ import shutil
 import os
 
 # shutil.copyfile("/Vaish_Flocking/init_poses.csv", "init_poses.csv")
-with open('/Vaish_Flocking/init_poses.csv', 'r') as f:
+with open('Vaish_Flocking/init_poses.csv', 'r') as f:
     init_pose_file = f.read()
 # print("initial file loaded")
 print("Copied")
