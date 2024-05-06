6ac3c610c18702b667e5d6dde5aa417eae9e0464
vdornadula <vaishnavidornadula2026@u.northwestern.edu>
2024-05-06 14:55:27 -0500

    removing os from path

--- get_files.py
+++ get_files.py
@@ -1,5 +1,5 @@
 import shutil
 import os
 
-shutil.copyfile(os.getcwd()+"/Vaish_Flocking/init_poses.csv", "init_poses.csv")
+shutil.copyfile("/Vaish_Flocking/init_poses.csv", "init_poses.csv")
 print("Copied")
