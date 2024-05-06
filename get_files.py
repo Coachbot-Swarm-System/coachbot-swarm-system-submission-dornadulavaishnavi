d3f594c721fde948f78e05432144d2004e60031c
vdornadula <vaishnavidornadula2026@u.northwestern.edu>
2024-05-06 15:00:25 -0500

    using with open instead of shutil copyfile

--- get_files.py
+++ get_files.py
@@ -1,5 +1,8 @@
 import shutil
 import os
 
-shutil.copyfile("/Vaish_Flocking/init_poses.csv", "init_poses.csv")
+# shutil.copyfile("/Vaish_Flocking/init_poses.csv", "init_poses.csv")
+with open('/Vaish_Flocking/init_poses.csv', 'r') as f:
+    init_pose_file = f.read()
+# print("initial file loaded")
 print("Copied")
