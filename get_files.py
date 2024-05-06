43bfa52657307a08789946e494204600b9493c83
vdornadula <vaishnavidornadula2026@u.northwestern.edu>
2024-05-06 15:35:27 -0500

    writing to local file

--- get_files.py
+++ get_files.py
@@ -1,8 +1,11 @@
 import shutil
 import os
 
-shutil.copyfile("/Vaish_Flocking/init_pose.csv", "init_poses.csv")
-# with open('Vaish_Flocking/init_pose.csv', 'r') as f:
-#     init_pose_file = f.read()
+init_file = open('init_pose.csv')
+# shutil.copyfile("/Vaish_Flocking/init_pose.csv", "init_poses.csv")
+with open('Vaish_Flocking/init_pose.csv', 'r') as f:
+    init_pose_file = f.read()
+    init_file.write(init_pose_file)
 # print("initial file loaded")
+init_file.close()
 print("Copied")
