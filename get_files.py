a8b30cff05fae7eead4ae6d0392d341a709d65a6
vdornadula <vaishnavidornadula2026@u.northwestern.edu>
2024-05-06 15:27:02 -0500

    back to shutil w correct file name

--- get_files.py
+++ get_files.py
@@ -1,8 +1,8 @@
 import shutil
 import os
 
-# shutil.copyfile("/Vaish_Flocking/init_poses.csv", "init_poses.csv")
-with open('Vaish_Flocking/init_pose.csv', 'r') as f:
-    init_pose_file = f.read()
+shutil.copyfile("/Vaish_Flocking/init_pose.csv", "init_poses.csv")
+# with open('Vaish_Flocking/init_pose.csv', 'r') as f:
+#     init_pose_file = f.read()
 # print("initial file loaded")
 print("Copied")
